c6f231864410d6b8063186249428fc6c9ed70cac
taylornovara <taylornovara@gmail.com>
2022-09-30 10:37:09 -0500

    Created the GUI window and added a background image

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# Constants
+"""
 
+A simple Pomodoro Timer app using tkinter for the GUI
+
+"""
+import tkinter
+
+# Constants
 PINK = "#FFC3C3"
 LITE_RED = "#FF8C8C"
 RED = "#FF5D5D"
@@ -8,4 +14,18 @@ FONT_NAME = ""
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 25
+
+# Creates program window
+window = tkinter.Tk()
+window.title("Pomodoro Timer")
+window.config(padx=100, pady=50)
+
+# Creates our background image and sets it to the center of the window
+canvas = tkinter.Canvas(width=200, height=224)
+img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=img)
+canvas.pack()
+
+# Keeps program window open
+window.mainloop()
 
