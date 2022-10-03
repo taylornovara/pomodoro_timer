6aff224e983e1a99ed04efb0ff8d892e3fa8b89b
taylornovara <taylornovara@gmail.com>
2022-10-03 13:11:09 -0500

    Updated background color and added text

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ PINK = "#FFC3C3"
 LITE_RED = "#FF8C8C"
 RED = "#FF5D5D"
 YELLOW = "#FFE3A9"
-FONT_NAME = ""
+FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 25
@@ -18,14 +18,14 @@ LONG_BREAK_MIN = 25
 # Creates program window
 window = tkinter.Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=LITE_RED)
 
-# Creates our background image and sets it to the center of the window
-canvas = tkinter.Canvas(width=200, height=224)
+# Creates our background image and txt. Sets both to the center of the window
+canvas = tkinter.Canvas(width=200, height=224, bg=LITE_RED, highlightthickness=0)
 img = tkinter.PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=img)
+canvas.create_image(100, 112, image=img)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 # Keeps program window open
 window.mainloop()
-
