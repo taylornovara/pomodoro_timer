de2f6d217faf75c8667a28a723df346d526abbff
taylornovara <taylornovara@gmail.com>
2022-10-06 10:49:19 -0500

    Updated countdown and labels

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ PINK = "#FFC3C3"
 LITE_RED = "#FF8C8C"
 RED = "#FF5D5D"
 YELLOW = "#FFE3A9"
+GREEN = '#59CE8F'
 FONT_NAME = ""
 WORK_MIN = 1
 SHORT_BREAK_MIN = 1
@@ -21,11 +22,22 @@ reps = 0
 # Functions
 
 def start_timer():
+    global reps
+    reps += 1
 
+    work_sec = WORK_MIN * 15
+    short_break_sec = SHORT_BREAK_MIN * 5
+    long_break_sec = LONG_BREAK_MIN * 10
 
-
-
-    countdown(5 * 60)
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        title_label.config(text="Long Break", fg=GREEN, bg=LITE_RED, font=(FONT_NAME, 40))
+    elif reps % 2 == 0:
+        countdown(work_sec)
+        title_label.config(text="Work", fg=RED, bg=LITE_RED, font=(FONT_NAME, 40))
+    elif reps % 2 != 0:
+        countdown(short_break_sec)
+        title_label.config(text="Short Break", fg=YELLOW)
 
 
 def countdown(count):
@@ -36,6 +48,17 @@ def countdown(count):
     canvas.itemconfig(timer, text=f"{minute}:{seconds}")
     if count > 0:
         window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_marks.config(text=marks)
+
+
+def reset_timer():
+    window.after_cancel()
 
 
 # Creates program window
@@ -58,11 +81,11 @@ canvas.grid(column=1, row=1)
 start_button = tkinter.Button(text="Start", font=(FONT_NAME, 18), bd=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tkinter.Button(text="Reset", font=(FONT_NAME, 18), bd=0)
+reset_button = tkinter.Button(text="Reset", font=(FONT_NAME, 18), bd=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 # Check Marks
-check_marks = tkinter.Label(text="✓", bg=LITE_RED, font=(FONT_NAME, 50))
+check_marks = tkinter.Label(bg=LITE_RED, font=(FONT_NAME, 50))
 check_marks.grid(column=1, row=3)
 
 # Keeps program window open
