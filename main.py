740a5f0eabecb428311d2544cb8a66fde0856022
taylornovara <taylornovara@gmail.com>
2022-10-06 09:36:21 -0500

    Updated countdown function to show two second digits

--- main.py
+++ main.py
@@ -4,16 +4,39 @@ A simple Pomodoro Timer app using tkinter for the GUI
 
 """
 import tkinter
+import math
 
 # Constants
 PINK = "#FFC3C3"
 LITE_RED = "#FF8C8C"
 RED = "#FF5D5D"
 YELLOW = "#FFE3A9"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 25
+FONT_NAME = ""
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
+reps = 0
+
+
+# Functions
+
+def start_timer():
+
+
+
+
+    countdown(5 * 60)
+
+
+def countdown(count):
+    minute = math.floor(count / 60)
+    seconds = count % 60
+    if seconds < 10:
+        seconds = f"0{seconds}"
+    canvas.itemconfig(timer, text=f"{minute}:{seconds}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # Creates program window
 window = tkinter.Tk()
@@ -28,11 +51,11 @@ title_label.grid(column=1, row=0)
 canvas = tkinter.Canvas(width=200, height=224, bg=LITE_RED, highlightthickness=0)
 img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img)
-canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+timer = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Buttons
-start_button = tkinter.Button(text="Start", font=(FONT_NAME, 18), bd=0)
+start_button = tkinter.Button(text="Start", font=(FONT_NAME, 18), bd=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = tkinter.Button(text="Reset", font=(FONT_NAME, 18), bd=0)
