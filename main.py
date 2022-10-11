a65c482724a855021c96a4bbf284b7831abfec7b
taylornovara <taylornovara@gmail.com>
2022-10-11 11:43:37 -0500

    Added colors, fixed timer bug, added a few notes, added reset timer function

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ PINK = "#FFC3C3"
 LITE_RED = "#FF8C8C"
 RED = "#FF5D5D"
 YELLOW = "#FFE3A9"
+DARK_GREEN = '#369B46'
 GREEN = '#59CE8F'
+WHITE = '#ffffff'
 FONT_NAME = ""
 WORK_MIN = 1
 SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 1
 reps = 0
+timer = None
 
 
 # Functions
@@ -31,25 +34,32 @@ def start_timer():
 
     if reps % 8 == 0:
         countdown(long_break_sec)
-        title_label.config(text="Long Break", fg=GREEN, bg=LITE_RED, font=(FONT_NAME, 40))
+        title_label.config(text="Break", fg=GREEN, bg=LITE_RED, font=(FONT_NAME, 40))
     elif reps % 2 == 0:
+        countdown(short_break_sec)
+        title_label.config(text="Break", fg=YELLOW)
+    elif reps % 2 != 0:
         countdown(work_sec)
         title_label.config(text="Work", fg=RED, bg=LITE_RED, font=(FONT_NAME, 40))
-    elif reps % 2 != 0:
-        countdown(short_break_sec)
-        title_label.config(text="Short Break", fg=YELLOW)
 
 
 def countdown(count):
+    """A function that receives the length and counts down to zero. Restarts as soon as it hits zero.
+    """
+    # Stores the minutes and seconds into their own variables.
     minute = math.floor(count / 60)
     seconds = count % 60
+
     if seconds < 10:
         seconds = f"0{seconds}"
-    canvas.itemconfig(timer, text=f"{minute}:{seconds}")
+    canvas.itemconfig(timer_text, text=f"{minute}:{seconds}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer
+        timer = window.after(1000, countdown, count - 1)
     else:
         start_timer()
+        # Divide reps by 2 to get the number of work sessions, add a check mark to marks, and configure check_marks
+        # label
         marks = ""
         work_sessions = math.floor(reps / 2)
         for _ in range(work_sessions):
@@ -58,7 +68,13 @@ def countdown(count):
 
 
 def reset_timer():
-    window.after_cancel()
+    """Reset all the check marks, text back to TIMER, and stop timer."""
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="TIMER", fg=WHITE)
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 
 # Creates program window
@@ -67,14 +83,14 @@ window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=LITE_RED)
 
 # Title
-title_label = tkinter.Label(text='TIMER', bg=LITE_RED, font=(FONT_NAME, 40))
+title_label = tkinter.Label(text='TIMER', fg=WHITE, bg=LITE_RED, font=(FONT_NAME, 40))
 title_label.grid(column=1, row=0)
 
 # Creates our background image and txt. Sets both to the center of the window
 canvas = tkinter.Canvas(width=200, height=224, bg=LITE_RED, highlightthickness=0)
 img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=img)
-timer = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 # Buttons
@@ -85,7 +101,7 @@ reset_button = tkinter.Button(text="Reset", font=(FONT_NAME, 18), bd=0, command=
 reset_button.grid(column=2, row=2)
 
 # Check Marks
-check_marks = tkinter.Label(bg=LITE_RED, font=(FONT_NAME, 50))
+check_marks = tkinter.Label(bg=LITE_RED, fg=RED, font=(FONT_NAME, 50))
 check_marks.grid(column=1, row=3)
 
 # Keeps program window open
